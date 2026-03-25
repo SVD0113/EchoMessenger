b22b7b0ac0ba45de485b50c02439d3eb801300ee
inseong choi <masil20203030@gmail.com>
2026-03-25 11:48:38 +0900

    과제4 코딩과 테스트 완료 커밋 오류로 재커밋

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// 필수 디자이너 변수입니다.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// 사용 중인 모든 리소스를 정리합니다.
-        /// </summary>
-        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,47 +15,50 @@
 
         #region Windows Form 디자이너에서 생성한 코드
 
-        /// <summary>
-        /// 디자이너 지원에 필요한 메서드입니다. 
-        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
-        /// </summary>
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.label1 = new System.Windows.Forms.Label();
+            this.lbTitle = new System.Windows.Forms.Label();
             this.lbMS = new System.Windows.Forms.ListBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.txtMS = new System.Windows.Forms.TextBox();
             this.btnSend = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAll = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // lbTitle
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("궁서", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.ForeColor = System.Drawing.Color.Blue;
-            this.label1.Location = new System.Drawing.Point(7, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(258, 27);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Echo Messenger";
+            this.lbTitle.Font = new System.Drawing.Font("궁서", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lbTitle.ForeColor = System.Drawing.Color.LightSkyBlue;
+            this.lbTitle.Location = new System.Drawing.Point(12, 9);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(456, 64);
+            this.lbTitle.TabIndex = 0;
+            this.lbTitle.Text = "Echo Messenger";
             // 
             // lbMS
             // 
+            this.lbMS.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.lbMS.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lbMS.ForeColor = System.Drawing.Color.White;
             this.lbMS.FormattingEnabled = true;
             this.lbMS.ItemHeight = 12;
-            this.lbMS.Location = new System.Drawing.Point(12, 39);
+            this.lbMS.Location = new System.Drawing.Point(12, 64);
             this.lbMS.Name = "lbMS";
-            this.lbMS.Size = new System.Drawing.Size(371, 328);
+            this.lbMS.Size = new System.Drawing.Size(434, 360);
             this.lbMS.TabIndex = 1;
+            this.lbMS.SelectedIndexChanged += new System.EventHandler(this.lbMS_SelectedIndexChanged);
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Right;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(389, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(464, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(408, 387);
+            this.pictureBox1.Size = new System.Drawing.Size(336, 450);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
@@ -70,37 +66,77 @@
             // 
             // txtMS
             // 
-            this.txtMS.BackColor = System.Drawing.SystemColors.Info;
-            this.txtMS.Location = new System.Drawing.Point(12, 369);
+            this.txtMS.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.txtMS.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMS.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtMS.ForeColor = System.Drawing.Color.White;
+            this.txtMS.Location = new System.Drawing.Point(12, 424);
             this.txtMS.Name = "txtMS";
-            this.txtMS.Size = new System.Drawing.Size(371, 21);
+            this.txtMS.Size = new System.Drawing.Size(434, 19);
             this.txtMS.TabIndex = 3;
             this.txtMS.Text = "(여기에 입력하세요)";
+            this.txtMS.Enter += new System.EventHandler(this.txtMS_Enter);
             this.txtMS.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtMS_KeyDown);
+            this.txtMS.Leave += new System.EventHandler(this.txtMS_Leave);
             // 
             // btnSend
             // 
-            this.btnSend.BackColor = System.Drawing.Color.Yellow;
-            this.btnSend.Location = new System.Drawing.Point(308, 367);
+            this.btnSend.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.btnSend.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSend.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnSend.ForeColor = System.Drawing.Color.LightSkyBlue;
+            this.btnSend.Location = new System.Drawing.Point(371, 424);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.Size = new System.Drawing.Size(75, 26);
             this.btnSend.TabIndex = 4;
             this.btnSend.Text = "전송";
             this.btnSend.UseVisualStyleBackColor = false;
             this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
             // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnDelete.ForeColor = System.Drawing.Color.LightSkyBlue;
+            this.btnDelete.Location = new System.Drawing.Point(464, 427);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "삭제";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnAll
+            // 
+            this.btnAll.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.btnAll.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAll.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnAll.ForeColor = System.Drawing.Color.LightSkyBlue;
+            this.btnAll.Location = new System.Drawing.Point(635, 427);
+            this.btnAll.Name = "btnAll";
+            this.btnAll.Size = new System.Drawing.Size(165, 26);
+            this.btnAll.TabIndex = 6;
+            this.btnAll.Text = "대화 기록 삭제";
+            this.btnAll.UseVisualStyleBackColor = false;
+            this.btnAll.Click += new System.EventHandler(this.btnAll_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(38)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btnSend);
-            this.Controls.Add(this.txtMS);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAll);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtMS);
             this.Controls.Add(this.lbMS);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbTitle);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -109,10 +145,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbTitle;
         private System.Windows.Forms.ListBox lbMS;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TextBox txtMS;
         private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAll;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,27 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace EchoMessenger
 {
     public partial class Form1 : Form
     {
+        string placeholderText = "     <입력창>     *여러줄 선택: Control+클릭 ";
+
         public Form1()
         {
             InitializeComponent();
+
+            txtMS.Text = placeholderText;
+            txtMS.ForeColor = Color.Gray;
+
+            // *** enter로 포커스 이동하면 (여기에 입력하세요)가 사라지지않는 문제 해결1
+            this.ActiveControl = lbTitle;
         }
 
         private void btnSend_Click(object sender, EventArgs e)
         {
-
-            if (string.IsNullOrWhiteSpace(txtMS.Text))
+            // 메시지 빈칸 전송 X
+            if (string.IsNullOrWhiteSpace(txtMS.Text) || txtMS.Text == placeholderText)
             {
+                return;
+            }
+
+            // 글자 수 제한 (50자)
+            if (txtMS.Text.Length > 50)
+            {
+                MessageBox.Show("메시지는 50자 미만으로 제한합니다");
                 return;
             }
 
@@ -35,15 +44,13 @@ namespace EchoMessenger
             lbMS.Items.Add(final_msg);
 
             // 메시지 카운팅
-            label1.Text = $"현재 대화: {lbMS.Items.Count}개";
+            lbTitle.Text = $"현재 대화: {lbMS.Items.Count}개";
 
-            // 보낸 tetMS 초기화
-            txtMS.Clear();
+            // 보낸 txtMS 초기화
+            txtMS.Text = "";
 
             // 전송하면 다시 txtMS로 포커스 이동
             txtMS.Focus();
-
-
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -64,6 +71,65 @@ namespace EchoMessenger
                 e.SuppressKeyPress = true;
                 btnSend.PerformClick();
             }
+        }
+
+        private void lbMS_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAll_Click(object sender, EventArgs e)
+        {
+            lbMS.Items.Clear();
+
+            // 카운팅 반영
+            lbTitle.Text = $"현재 대화: {lbMS.Items.Count}개";
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // lbMS.SelectedItems.Count는 '현재 파란색으로 선택된 항목의 개수'입니다.
+            // 즉, 1개 이상 선택했다면 삭제를 진행하고, 0개면 경고창을 띄웁니다.
+            if (lbMS.SelectedItems.Count > 0)
+            {
+                // [핵심 로직] 선택된 항목이 0개가 될 때까지 계속 반복해서 지웁니다.
+                while (lbMS.SelectedItems.Count > 0)
+                {
+                    // 선택된 것들 중 제일 첫 번째(0번)를 리스트박스에서 완전히 지워버립니다.
+                    lbMS.Items.Remove(lbMS.SelectedItems[0]);
+                }
+
+                // 다 지웠으니 메시지 개수(카운팅)를 다시 새로고침 해줍니다.
+                lbTitle.Text = $"현재 대화: {lbMS.Items.Count}개";
+            }
+            else
+            {
+                // 아무것도 선택 안 하고 삭제 버튼을 눌렀을 때
+                MessageBox.Show("삭제할 메시지를 먼저 클릭해주세요");
+            }
+        }
+
+        private void txtMS_Enter(object sender, EventArgs e)
+        {
+            if (txtMS.Text == placeholderText)
+            {
+                txtMS.Text = "";
+                txtMS.ForeColor = Color.White;
+            }
+        }
+
+        private void txtMS_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtMS.Text))
+            {
+                txtMS.Text = placeholderText;
+                txtMS.ForeColor = Color.Gray;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
